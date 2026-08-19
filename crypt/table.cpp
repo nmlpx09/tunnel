da108d743ce10fd1bc497ca204e61e4da2245757
nmlpx <nmlpx09@duck.com>
2026-08-19 16:50:04 +0300

    fix

--- crypt/table.cpp
+++ crypt/table.cpp
@@ -48,16 +48,24 @@ std::error_code TTable::Init(const std::string&) {
 }
 
 TBufferView TTable::Encrypt(TBufferView buffer) noexcept {
+    if (buffer.size() > EncBuffer.size()) {
+        return {};
+    }
+
     std::size_t index = 0;
-    for (; index < EncBuffer.size() && index < buffer.size(); ++index) {
+    for (; index < buffer.size(); ++index) {
         EncBuffer[index] = EncryptTable[buffer[index]];
     }
     return {EncBuffer.begin(), index};
 }
 
 TBufferView TTable::Decrypt(TBufferView buffer) noexcept {
+    if (buffer.size() > DecBuffer.size()) {
+        return {};
+    }
+
     std::size_t index = 0;
-    for (; index < DecBuffer.size() && index < buffer.size(); ++index) {
+    for (; index < buffer.size(); ++index) {
         DecBuffer[index] = DecryptTable[buffer[index]];
     }
     return {DecBuffer.begin(), index};
